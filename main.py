af3212bbd27ee18bfdab1c405908a5017bc0ce59
Jake Wehder <108289690+JWehder@users.noreply.github.com>
2023-10-16 12:34:24 -0400

    building out the player

--- main.py
+++ main.py
@@ -6,14 +6,27 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
+BG = pygame.transform.scale(pygame.image.load("cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3B4MTM2NjcxMC1pbWFnZS1rd3Z4eGVxcC5qcGc.webp"), (WIDTH, HEIGHT))
+
+PLAYER_WIDTH = 40
+PLAYER_WIDTH = 60
+
+def draw():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
+
 def main():
     run = True
+
+    player = pygame.Rect()
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw()
 
     pygame.quit()
 
